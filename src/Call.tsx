f8c6a6b989a66ebd589a852f77e407e26a75b3bf
FukoMaster <sjx95@outlook.com>
2022-05-28 21:33:16 +0800

    stash

--- src/Call.tsx
+++ src/Call.tsx
@@ -1,30 +1,12 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
-import AgoraRTC, { IAgoraRTCRemoteUser, ILocalAudioTrack, ILocalTrack, ILocalVideoTrack } from 'agora-rtc-sdk-ng';
-import useAgora from './hooks/useAgora';
+import React, { useEffect, useState } from 'react';
+import { IAgoraRTCClient, IAgoraRTCRemoteUser, ILocalAudioTrack, ILocalTrack, ILocalVideoTrack } from 'agora-rtc-sdk-ng';
 import MediaPlayer from './components/MediaPlayer';
-import './Call.css';
 import 'agora-access-token';
-import { RtcRole, RtcTokenBuilder, RtmTokenBuilder, RtmRole } from 'agora-access-token';
-import useAgoraRTM from './hooks/useAgoraRTM';
-import { Button, Col, Container, Dropdown, FloatingLabel, Form, Row } from 'react-bootstrap';
-import ThemeProvider, { useBootstrapBreakpoints } from 'react-bootstrap/esm/ThemeProvider';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useWrappedState } from './hooks/useWrappedStates';
+import { RtmChannel, RtmClient } from 'agora-rtm-sdk';
+import { ChatController } from './ChatController';
 
-const client = AgoraRTC.createClient({ codec: 'h264', mode: 'rtc' });
-const appID = process.env.REACT_APP_AGORA_APP_ID ?? ""
-const appCertificate = process.env.REACT_APP_AGORA_APP_CERTIFICATE ?? ""
-const userID = Math.floor(Math.random() * (1 << 31 - 1));
-
-function genRTCToken(channelName: string): string {
-  const uid = 0;
-  const role = RtcRole.PUBLISHER;
-  const privilegeExpiredTs = 0
-  return RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs)
-}
-
-function genRTMToken(): string {
-  const token = RtmTokenBuilder.buildToken(appID, appCertificate, userID.toString(), RtmRole.Rtm_User, 86400);
-  return token;
-}
 
 function useTrackControl(joinState: boolean, track: ILocalTrack | undefined) {
   const [checked, setChecked] = useState(true);
@@ -47,28 +29,33 @@ function useTrackControl(joinState: boolean, track: ILocalTrack | undefined) {
   return { checked, disabled, onChange }
 }
 
-interface ICallStates {
-  channel: { value: string, onChange: ((value: string) => void) }
-}
-
 export default function Call() {
 
-  const [channel, setChannel] = useState('test-web-room');
-  const [message, setMessage] = useState('');
-  const [localAudioTrack, setLocalAudioTrack] = useState<ILocalAudioTrack | undefined>();
-  const [localVideoTrack, setLocalVideoTrack] = useState<ILocalVideoTrack>();
-  const [rtcRemoteUsers, setRTCRemoteUsers] = useState<IAgoraRTCRemoteUser[]>([]);
-  const { leave, join, joinState, remoteUsers } = useAgora(client);
-  const rtm = useAgoraRTM();
+  const gs = {
+    channel: useWrappedState('test-web-room'),
+    videoInputDevice: useWrappedState(''),
+    audioInputDevice: useWrappedState('default'),
+    audioOutputDevice: useWrappedState('default'),
+    subscribeVideo: useWrappedState(true),
+    subscribeAudio: useWrappedState(true),
+    rtcRemoteUsers: useWrappedState<IAgoraRTCRemoteUser[]>([]),
+
+    joined: useWrappedState(false),
+
+    videoClient: useWrappedState<IAgoraRTCClient | undefined>(undefined),
+    msgClient: useWrappedState<RtmClient | undefined>(undefined),
+    msgChannel: useWrappedState<RtmChannel | undefined>(undefined),
+
+  };
 
   return (
     <Container fluid={true}>
       <Row>
         <Col lg={3}>
-          <ChatController setLocalAudioTrack={setLocalAudioTrack} setLocalVideoTrack={setLocalVideoTrack} setRTCRemoteUsers={setRTCRemoteUsers} />
+          <ChatController {...gs} />
         </Col>
         <Col lg={6}>
-          <ChatVideos userID={userID} />
+          <ChatVideos userID={0} />
         </Col>
         <Col lg={3}>
           <ChatTexts sendMessage={() => { }} />
@@ -79,92 +66,7 @@ export default function Call() {
   );
 }
 
-interface ChatControllerProps {
-  setLocalAudioTrack: (track: ILocalAudioTrack) => void,
-  setLocalVideoTrack: (track: ILocalVideoTrack) => void,
-  setRTCRemoteUsers: Dispatch<SetStateAction<IAgoraRTCRemoteUser[]>>,
-};
 
-function ChatController(props: ChatControllerProps) {
-
-  const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
-  useEffect(() => { updateDevices(); }, [])
-
-  async function updateDevices() {
-    const ds = await AgoraRTC.getDevices();
-    console.log('AgoraRTC.getDevices():', ds);
-    setDevices(ds);
-  }
-
-  return (
-    <Form>
-      <Row>
-        <Col> Room Control</Col>
-      </Row>
-      <Row>
-        <Col xs={6} md={8} lg={12}>
-          <FloatingLabel label='Channel' controlId='channel'>
-            <Form.Control placeholder="test-web-room" />
-          </FloatingLabel>
-        </Col>
-        <Col xs={6} md={4} lg={12}>
-          <Row xs={2} style={{ height: '100%' }}>
-            <Col>
-              <Button style={{ height: '100%', width: '100%', alignItems: 'stretch' }}>
-                Join
-              </Button>
-            </Col>
-            <Col>
-              <Button style={{ height: '100%', width: '100%', alignItems: 'stretch' }}>
-                Leave
-              </Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row>
-        <Col> Local Device Control</Col>
-      </Row>
-      <Row xs={2} md={4} lg={1} style={{ alignItems: 'stretch' }}>
-        <Col>
-          <Button style={{ height: '100%', width: '100%', alignItems: 'stretch' }}>
-            Refresh Devices
-          </Button>
-        </Col>
-        <FloatingLabel as={Col} label='Video Input'>
-          <Form.Select>
-            <option value=""> Disable </option>
-            {devices.filter((d) => { return d.kind === 'videoinput' }).
-              map((d) => { return (<option key={d.deviceId} value={d.deviceId}> {d.label} </option>) })}
-          </Form.Select>
-        </FloatingLabel>
-        <FloatingLabel as={Col} label='Audio Input'>
-          <Form.Select value={"default"}>
-            <option value=""> Mute </option>
-            {devices.filter((d) => { return d.kind === 'audioinput' }).
-              map((d) => { return (<option key={d.deviceId} value={d.deviceId}> {d.label} </option>) })}
-          </Form.Select>
-        </FloatingLabel>
-        <FloatingLabel as={Col} label='Audio Output'>
-          <Form.Select value={"default"}>
-            <option value=""> Mute </option>
-            {devices.filter((d) => { return d.kind === 'audiooutput' }).
-              map((d) => { return (<option key={d.deviceId} value={d.deviceId}> {d.label} </option>) })}
-          </Form.Select>
-        </FloatingLabel>
-      </Row>
-      <Row>
-        <Col> Advance Control</Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Check type='switch' label={`Auto Subscribe Video`} />
-          <Form.Check type='switch' label={`Auto Subscribe Audio`} />
-        </Col>
-      </Row>
-    </Form>
-  )
-}
 
 export interface ChatVideosProps {
   userID?: number
@@ -202,73 +104,11 @@ function ChatTexts(props: ChatTextsProps) {
     <div>
       {props.history?.map(str => <div>{str}</div>)}
       <div>
-        {userID}:
+        {props.userID}:
         <input type='text' value={message} onChange={(event) => setMessage(event.target.value)} />
         <button onClick={() => { props.sendMessage(message); setMessage(''); }}>
           Send
         </button>
-      </div>
-    </div>
-  )
-}
-
-
-function VideosB(props: any) {
-  const [channel, setChannel] = useState('test-web-room');
-  const [message, setMessage] = useState('');
-  const { localAudioTrack, localVideoTrack, leave, join, joinState, remoteUsers } = useAgora(client);
-  const rtm = useAgoraRTM();
-  console.log(props.channel);
-  return (
-    <div className='call'>
-      <form className='call-form'>
-        <label>
-          Channel:
-          <input value={channel} type='text' name='channel' onChange={(event) => { setChannel(event.target.value) }} />
-        </label>
-        <label>
-          Audio:
-          <input type='checkbox' {...useTrackControl(joinState, localAudioTrack)} />
-        </label>
-        <label>
-          Video:
-          <input type='checkbox' {...useTrackControl(joinState, localVideoTrack)} />
-        </label>
-        <div className='button-group'>
-          <label>RTC Online: <input type='checkbox' disabled={true} checked={joinState} /></label>
-          <label>RTM Online: <input type='checkbox' disabled={true} checked={rtm.joinState} /></label>
-          <button id='join' type='button' className='btn btn-primary btn-sm' disabled={joinState || rtm.joinState}
-            onClick={async () => {
-              await join(appID, channel, genRTCToken(channel), userID);
-              await rtm.join(appID, channel, userID.toString(), genRTMToken());
-            }}>Join</button>
-          <button id='leave' type='button' className='btn btn-primary btn-sm' disabled={!joinState && !rtm.joinState}
-            onClick={async () => {
-              await rtm.leave();
-              await leave();
-            }}>Leave</button>
-        </div>
-      </form>
-      <div className='rtc-container'>
-        <div className='local-player-wrapper'>
-          <p className='local-player-text'>{localVideoTrack && `localTrack`}{joinState && localVideoTrack ? `(${client.uid})` : ''}</p>
-          <MediaPlayer videoTrack={localVideoTrack} audioTrack={localAudioTrack} disableAudio={true} />
-        </div>
-        {remoteUsers.map(user => (
-          <div className='remote-player-wrapper' key={user.uid}>
-            <p className='remote-player-text'>{`remoteVideo(${user.uid})`}</p>
-            <MediaPlayer videoTrack={user.videoTrack} audioTrack={user.audioTrack} />
-          </div>
-        ))}
-      </div>
-      <div className='rtm-container'>
-        {rtm.history.map(str => <div>{str}</div>)}
-        <div>
-          {userID}:
-          <input type='text' value={message} onChange={(event) => setMessage(event.target.value)} />
-          <button disabled={!rtm.joinState}
-            onClick={() => { rtm.sendMessage(message); setMessage(''); }}> Send </button>
-        </div>
       </div>
     </div>
   )
---
+++ src/ChatController.tsx
@@ -0,0 +1,156 @@
+import { RtcRole, RtcTokenBuilder, RtmTokenBuilder, RtmRole } from "agora-access-token"
+import AgoraRTC, { IAgoraRTCClient } from "agora-rtc-sdk-ng"
+import AgoraRTM, { RtmClient, RtmChannel } from "agora-rtm-sdk"
+import { useState, useEffect } from "react"
+import { Form, Row, Col, FloatingLabel, Button } from "react-bootstrap"
+import { IWrappedState } from "./hooks/useWrappedStates"
+
+const appID = process.env.REACT_APP_AGORA_APP_ID ?? ""
+const appCertificate = process.env.REACT_APP_AGORA_APP_CERTIFICATE ?? ""
+
+const client = AgoraRTC.createClient({ codec: 'h264', mode: 'rtc' });
+const msgClient = AgoraRTM.createInstance(appID);
+
+const userID = Math.floor(Math.random() * (1 << 31 - 1));
+
+function genRTCToken(channelName: string): string {
+  const uid = 0;
+  const role = RtcRole.PUBLISHER;
+  const privilegeExpiredTs = 0
+  return RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs)
+}
+
+function genRTMToken(): string {
+  const token = RtmTokenBuilder.buildToken(appID, appCertificate, userID.toString(), RtmRole.Rtm_User, 86400);
+  return token;
+}
+
+interface ChatControllerProps {
+  channel: IWrappedState<string>
+  joined: IWrappedState<boolean>
+  videoClient: IWrappedState<IAgoraRTCClient | undefined>
+  msgClient: IWrappedState<RtmClient | undefined>
+  msgChannel: IWrappedState<RtmChannel | undefined>
+};
+
+export function ChatController(props: ChatControllerProps) {
+
+  const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+  useEffect(() => { updateDevices(); }, [])
+  async function updateDevices() {
+    const ds = await AgoraRTC.getDevices();
+    setDevices(ds);
+  }
+
+  const [joining, setJoining] = useState(false);
+
+
+  async function join() {
+    // if (!props.videoClient.value || !props.msgClient.value) return;
+
+    const channelName = props.channel.value;
+
+    await client.join(appID, channelName, genRTCToken(channelName), userID);
+    props.videoClient.set(client);
+
+    await msgClient.login({ uid: userID.toString(), token: genRTMToken() });
+    props.msgClient.set(msgClient);
+
+    const channel = msgClient.createChannel(channelName);
+    await channel.join();
+
+    props.msgChannel.set(channel);
+    props.joined.set(true);
+  }
+
+  async function leave() {
+    await props.msgChannel.value?.leave();
+    props.msgChannel.set(undefined);
+
+    await props.msgClient.value?.logout();
+    props.msgClient.set(undefined);
+
+    await props.videoClient.value?.leave();
+    props.videoClient.set(undefined);
+
+    props.joined.set(false);
+  }
+
+  return (
+    <Form>
+      <Row>
+        <Col> Room Control</Col>
+      </Row>
+      <Row>
+        <Col xs={6} md={8} lg={12}>
+          <FloatingLabel label='Channel' controlId='channel'>
+            <Form.Control placeholder="test-web-room"
+              value={props.channel.value}
+              onChange={(e) => { props.channel.set(e.target.value) }}
+              disabled={props.joined.value} />
+          </FloatingLabel>
+        </Col>
+        <Col xs={6} md={4} lg={12}>
+          <Row xs={2} style={{ height: '100%' }}>
+            <Col>
+              <Button style={{ height: '100%', width: '100%', alignItems: 'stretch' }}
+                onClick={join}
+                disabled={props.joined.value}>
+                Join
+              </Button>
+            </Col>
+            <Col>
+              <Button style={{ height: '100%', width: '100%', alignItems: 'stretch' }}
+                onClick={leave}
+                disabled={!props.joined.value}>
+                Leave
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row >
+      <Row>
+        <Col> Local Device Control</Col>
+      </Row>
+      <Row xs={2} md={4} lg={1} style={{ alignItems: 'stretch' }}>
+        <Col>
+          <Button style={{ height: '100%', width: '100%', alignItems: 'stretch' }}>
+            Refresh Devices
+          </Button>
+        </Col>
+        <FloatingLabel as={Col} label='Video Input'>
+          <Form.Select>
+            <option value=""> Disable </option>
+            {devices.filter((d) => { return d.kind === 'videoinput' }).
+              map((d) => { return (<option key={d.deviceId} value={d.deviceId}> {d.label} </option>) })}
+          </Form.Select>
+        </FloatingLabel>
+        <FloatingLabel as={Col} label='Audio Input'>
+          <Form.Select value={"default"}>
+            <option value=""> Mute </option>
+            {devices.filter((d) => { return d.kind === 'audioinput' }).
+              map((d) => { return (<option key={d.deviceId} value={d.deviceId}> {d.label} </option>) })}
+          </Form.Select>
+        </FloatingLabel>
+        <FloatingLabel as={Col} label='Audio Output'>
+          <Form.Select value={"default"}>
+            <option value=""> Mute </option>
+            {devices.filter((d) => { return d.kind === 'audiooutput' }).
+              map((d) => { return (<option key={d.deviceId} value={d.deviceId}> {d.label} </option>) })}
+          </Form.Select>
+        </FloatingLabel>
+      </Row>
+      <Row>
+        <Col> Advance Control</Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Switch label={`Mute Local Audio`} />
+          <Form.Switch label={`Mute Remote Audio`} />
+          <Form.Switch label={`Subscribe Remote Video`} />
+          <Form.Switch label={`Subscribe Remote Audio`} />
+        </Col>
+      </Row>
+    </Form >
+  )
+}
