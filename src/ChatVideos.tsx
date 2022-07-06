332268a79ef41f7820c0240e098a3371704e6d07
FukoMaster <sjx95@outlook.com>
2022-07-06 22:52:13 +0800

    feat: show local network quality and remote e2e latency (#9)
    * feat: show latancy of remote users
    
    * feat: show quality of local network

--- src/ChatVideos.tsx
+++ src/ChatVideos.tsx
@@ -1,5 +1,8 @@
-import { ILocalAudioTrack, ILocalVideoTrack, IAgoraRTCRemoteUser, IAgoraRTCClient } from "agora-rtc-sdk-ng"
+import { ILocalAudioTrack, ILocalVideoTrack, IAgoraRTCRemoteUser, IAgoraRTCClient, NetworkQuality } from "agora-rtc-sdk-ng"
+import { ReactElement, useEffect, useState } from "react"
+import { Badge } from "react-bootstrap"
 import MediaPlayer from "./components/MediaPlayer"
+import useAgroaRTCQuality from "./hooks/useAgoraRTCQuality"
 
 export interface ChatVideosProps {
     userID?: number
@@ -11,18 +14,78 @@ export interface ChatVideosProps {
 
 export function ChatVideos(props: ChatVideosProps) {
 
+    const remoteQualities = useAgroaRTCQuality(props.videoClient);
+
+    const remotes = props.remoteUsers?.map((user) => {
+        const rq = remoteQualities.get(user.uid);
+        const e2eDelay = Math.max(rq?.AudioStats?.end2EndDelay ?? 0, rq?.AudioStats?.end2EndDelay ?? 0);
+        const netDelay = Math.max(rq?.AudioStats?.transportDelay ?? 0, rq?.AudioStats?.transportDelay ?? 0);
+        return {
+            user, e2eDelay, netDelay,
+            audioE2EDelay: rq?.AudioStats?.end2EndDelay
+        };
+    });
+
+    const [localBadge, setLocalBadge] = useState<ReactElement>();
+    useEffect(() => {
+        if (!props.videoClient) {
+            setLocalBadge(undefined);
+            return
+        };
+
+        const listener = (stats: NetworkQuality) => {
+            const quality = Math.max(stats.uplinkNetworkQuality, stats.downlinkNetworkQuality);
+            let text: string = 'unknown';
+            let color: string = 'secondary';
+
+            if (quality === 0) {
+                text = 'unknown';
+                color = 'secondary';
+            } else if (quality <= 2) {
+                text = 'great';
+                color = 'success';
+            } else if (quality <= 4) {
+                text = 'good';
+                color = 'warning';
+            } else {
+                text = 'poor';
+                color = 'danger';
+            };
+            setLocalBadge(<Badge bg={color}>network: {text}({quality})</Badge>)
+        };
+        props.videoClient.on('network-quality', listener);
+        return () => { props.videoClient?.off('network-quality', listener) }
+    }, [props.videoClient, setLocalBadge]);
+
+    const latencyColor = (latency: number | undefined): string => {
+        if (latency === undefined) return 'secondary';
+        if (latency > 300) return 'danger';
+        if (latency > 150) return 'warning';
+        return 'success';
+    };
+
     return (
         <div>
             <div className='local-player-wrapper'>
-                <p className='local-player-text'> localTrack({props.userID}) </p>
+                <p className='local-player-text'>
+                    <span>localTrack({props.userID})</span>
+                    {localBadge}
+                </p>
                 <MediaPlayer
                     videoTrack={props.localVideoTrack} audioTrack={props.localAudioTrack}
                     gainRange={[-40, 20]} disableAudio={true} />
             </div>
-            {props.remoteUsers?.map(user => (
-                <div className='remote-player-wrapper' key={user.uid}>
-                    <p className='remote-player-text'>{`remoteVideo(${user.uid})`}</p>
-                    <MediaPlayer videoTrack={user.videoTrack} audioTrack={user.audioTrack} />
+            {remotes?.map(remote => (
+                <div className='remote-player-wrapper' key={remote.user.uid}>
+                    <p className='remote-player-text'>
+                        <span>
+                            {`remoteVideo(${remote.user.uid})`}
+                        </span>
+                        <Badge bg={latencyColor(remote.audioE2EDelay)}>
+                            latency: {remote.audioE2EDelay}ms
+                        </Badge>
+                    </p>
+                    <MediaPlayer videoTrack={remote.user.videoTrack} audioTrack={remote.user.audioTrack} />
                 </div>
             ))}
         </div>
